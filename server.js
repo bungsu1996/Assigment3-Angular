6621927e5a2ec62a7aef1c068ccba8b82193fe89
bungsu1996 <venomous.zach@gmail.com>
2021-12-21 05:28:14 +0700

    deploy-update 3

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static());
+app.use(express.static("./dist/e-comm"));
 
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "" });
+  res.sendFile("index.html", { root: "dist/e-comm/" });
 });
 
 app.listen(process.env.PORT || 4200);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  basedUrlBackendUser: 'https://backendass3.herokuapp.com/albarrmart',
+  basedUrlBackendProductList:
+    'https://backendass3.herokuapp.com/albarrmart/listItem',
+  basedUrlBackendProductPilihan:
+    'https://backendass3.herokuapp.com/albarrmart/listItem/productpilihan2',
+  basedUrlBackendProductPilihan2:
+    'https://backendass3.herokuapp.com/albarrmart/listItem/productpilihan',
 };
